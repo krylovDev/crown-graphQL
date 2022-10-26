dabd7995d1c922942a06116a12a9f77868b98d74
Vladimir <vladimirkrylov@gmail.com>
2022-10-26 21:23:08 +0300

    Initial commit

---
+++ src/routes/categories-preview/categories-preview.component.jsx
@@ -0,0 +1,27 @@
+import { useContext, Fragment } from 'react';
+import Spinner from "../../components/spinner/spinner.component";
+
+import { CategoriesContext } from '../../contexts/categories.context';
+import CategoryPreview from '../../components/category-preview/category-preview.component';
+
+const CategoriesPreview = () => {
+	const {categoriesMap, loading} = useContext(CategoriesContext);
+
+	return (
+		<Fragment>
+			{ loading
+					? <Spinner />
+					: (
+						Object.keys(categoriesMap).map((title) => {
+							const products = categoriesMap[title];
+							return (
+								<CategoryPreview key={title} title={title} products={products}/>
+							);
+						})
+					)
+			}
+		</Fragment>
+	);
+};
+
+export default CategoriesPreview;
